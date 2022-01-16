ede41d3d1a139fb4208c371e1e5f6ae86175a5b0
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2022-01-16 15:48:44 +0200

    feat: Initialize RotatingShape

---
+++ src/RotatingShape.mjs
@@ -0,0 +1,19 @@
+export class RotatingShape {
+  shape;
+
+  constructor(shape) {
+    this.shape = shape;
+  }
+
+  toString() {
+    throw new Error("Not implemented");
+  }
+
+  rotateRight() {
+    throw new Error("Not implemented");
+  }
+
+  rotateLeft() {
+    throw new Error("Not implemented");
+  }
+}
