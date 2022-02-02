0fca72a6cf5df2a55974a252ba5fb07e19dc76fd
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2022-02-02 21:55:45 +0200

    fix: Pass rotateLeft test for I

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -5,16 +5,22 @@ export class Tetromino extends RotatingShape {
     super(shape);
   }
 
-  static T_SHAPE = new RotatingShape(
+  static T_SHAPE = new Tetromino(
     `.T.
     TTT
     ...`
   );
-  static I_SHAPE = new RotatingShape(
+
+  static I_SHAPE = new Tetromino(
     `.....
     .....
     IIII.
     .....
     .....`
   );
+
+  rotateLeft() {
+    if (this.shape === Tetromino.I_SHAPE.shape) return super.rotateRight();
+    else return super.rotateLeft();
+  }
 }
