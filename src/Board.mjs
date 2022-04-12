898bc6e386f118915d0bb6fdd0973951d3f6abf4
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2022-04-12 17:57:00 +0300

    Pass tetromino drop test

--- src/Board.mjs
+++ src/Board.mjs
@@ -29,11 +29,45 @@ export class Board {
 
     this.hasFallingBlocks = true;
 
-    const fallingRow = this.row
-      .slice(0, this.middleIndex)
-      .concat(block.color, this.row.slice(this.middleIndex + 1, this.width));
-    this.board[0] = fallingRow;
-    this.currentBlockRow = 0;
+    if (block.color) {
+      // Shape is 1x1
+      const fallingRow = this.row
+        .slice(0, this.middleIndex)
+        .concat(block.color, this.row.slice(this.middleIndex + 1, this.width));
+      this.board[0] = fallingRow;
+      this.currentBlockRow = 0;
+    } else if (block.shape) {
+      // Shape is a Tetromino
+      const padBlockRow = (blockRow) => {
+        const paddedRow = this.row
+          .slice(0, Math.ceil(blockRow.length / 2) + 1)
+          .concat(
+            blockRow.join(""),
+            this.row.slice(
+              this.middleIndex + Math.floor(blockRow.length / 2),
+              this.width
+            )
+          );
+
+        if (paddedRow.length > this.row.length) {
+          throw new Error(
+            `Too much padding for Tetromino: expected ${this.row.length}, got ${paddedRow.length}`
+          );
+        } else if (paddedRow.length < this.row.length) {
+          throw new Error(
+            `Too little padding for Tetromino: expected ${this.row.length}, got ${paddedRow.length}`
+          );
+        }
+        return paddedRow;
+      };
+      // Pad tetromino rows to shape
+      const blockSplit = block.shape.map((blockRow) => padBlockRow(blockRow));
+
+      // Replace first Tetromino.height = no of rows with paddeds̄
+      const blockHeight = blockSplit.length;
+      this.board = blockSplit.concat(this.board.slice(blockHeight));
+      this.currentBlockRow = 0;
+    }
   }
 
   tick() {
