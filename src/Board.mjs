e1f525c49d2ed34889ab87d6ed7e5341b9c8074f
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2022-05-05 17:36:57 +0300

    wip: Stop when tetromino hits bottom

--- src/Board.mjs
+++ src/Board.mjs
@@ -12,6 +12,7 @@ export class Board {
 
     this.hasFallingBlocks = false;
     this.currentBlockRow = 0;
+    this.currentBlockHeight = 0;
   }
 
   toString() {
@@ -28,6 +29,7 @@ export class Board {
     }
 
     this.hasFallingBlocks = true;
+    this.currentBlock = block;
 
     if (block.color) {
       // Shape is 1x1
@@ -36,6 +38,7 @@ export class Board {
         .concat(block.color, this.row.slice(this.middleIndex + 1, this.width));
       this.board[0] = fallingRow;
       this.currentBlockRow = 0;
+      this.currentBlockHeight = block.color.length;
     } else if (block.shape) {
       // Shape is a Tetromino
       const padBlockRow = (blockRow) => {
@@ -67,6 +70,7 @@ export class Board {
       const blockHeight = blockSplit.length;
       this.board = blockSplit.concat(this.board.slice(blockHeight));
       this.currentBlockRow = 0;
+      this.currentBlockHeight = blockHeight;
     }
   }
 
@@ -75,12 +79,28 @@ export class Board {
       // Last row
       this.currentBlockRow == this.height - 1 ||
       // Stop when there is something on the bottom
+      this.board[this.currentBlockRow + this.currentBlockHeight] !== this.row
     ) {
       this.hasFallingBlocks = false;
     }
 
     if (this.hasFallingBlocks) {
-      this.board[this.currentBlockRow + 1] = this.board[this.currentBlockRow];
+      // const currentBlockRowEnd = this.currentBlockRow + this.currentBlockHeight;
+      // const blockRows = this.board.slice(
+      //   this.currentBlockRow,
+      //   currentBlockRowEnd
+      // );
+
+      // const newBoard = this.board
+      //   .slice(0, this.currentBlockRow)
+      //   .concat(this.row)
+      //   .concat(blockRows)
+      //   .concat(
+      //     this.board.slice(currentBlockRowEnd + 1, this.board.length - 2)
+      //   );
+
+      this.board[this.currentBlockRow + this.currentBlockHeight] =
+        this.board[this.currentBlockRow];
       if (this.currentBlockRow == 0) {
         this.board[this.currentBlockRow] = this.row;
       } else {
