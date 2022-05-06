3101cf804c292096d5bbf08d233d350e4a0edfaf
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2022-05-06 14:21:26 +0300

    Initialize moving tests

---
+++ test/MovingFallingTetrominos.test.mjs
@@ -0,0 +1,41 @@
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+describe("Moving tetrominoes", () => {
+  it("can be moved right", () => {
+    throw new Error("Test not implemented");
+  });
+
+  xit("can be moved left", () => {
+    throw new Error("Test not implemented");
+  });
+
+  xit("can be moved down", () => {
+    throw new Error("Test not implemented");
+  });
+
+  xit("cannot be moved left beyond the board", () => {
+    throw new Error("Test not implemented");
+  });
+
+  xit("cannot be moved right beyond the board", () => {
+    throw new Error("Test not implemented");
+  });
+
+  xit("cannot be moved down beyond the board (will stop falling)", () => {
+    throw new Error("Test not implemented");
+  });
+
+  xit("cannot be moved left through other blocks", () => {
+    throw new Error("Test not implemented");
+  });
+
+  xit("cannot be moved right through other blocks", () => {
+    throw new Error("Test not implemented");
+  });
+
+  xit("cannot be moved down through other blocks (will stop falling)", () => {
+    throw new Error("Test not implemented");
+  });
+});
